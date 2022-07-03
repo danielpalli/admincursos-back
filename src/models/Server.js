b2461125962847756c85a5fd4987023105501d38
danielpalli <dpallicolque@est.frba.utn.edu.ar>
2022-07-03 14:05:44 -0300

    update

--- src/models/Server.js
+++ src/models/Server.js
@@ -5,17 +5,18 @@ export class Server {
   constructor() {
 
     this.app = express();
-    this.config();
+    this.app.set('port', 4000);
+    this.middlewares();
     this.routes();
   }
 
-  config() {
-    this.app.set('port', 4000);
+  middlewares() {
+    this.app.use(express.static('public'));
+    this.app.use(express.json());
     this.app.use(cors());
-  }
+}
 
   routes() {
-    this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: false }));
   }
 
