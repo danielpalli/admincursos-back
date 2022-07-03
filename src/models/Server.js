7a1dcf1353a21e9e84e90676494631a071e457e5
danielpalli <dpallicolque@est.frba.utn.edu.ar>
2022-07-03 14:56:38 -0300

    update:

--- src/models/Server.js
+++ src/models/Server.js
@@ -7,7 +7,7 @@ export class Server {
   constructor() {
     this.app = express();
     this.port = 4000;
-    this.usuariosPath = '/api/usuarios';
+    this.usuariosPath = '/api/usuarios/';
     this.databaseConnect();
     this.middlewares();
     this.routes();
